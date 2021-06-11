1082358d3cbc1472e17c8a8a7b4b2ef41dc787a3
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:45:23 +0200

    Initial commit

---
+++ CS007Kalkulacka/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace CS007Kalkulacka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.buttonDeleno = new System.Windows.Forms.Button();
+            this.buttonKrat = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonVymazat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.textBox.Location = new System.Drawing.Point(22, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(303, 44);
+            this.textBox.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 61);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 61);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 61);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(95, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 61);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(168, 133);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 61);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(22, 66);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(67, 61);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(95, 66);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(67, 61);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(168, 66);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(67, 61);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleno
+            // 
+            this.buttonDeleno.Location = new System.Drawing.Point(258, 66);
+            this.buttonDeleno.Name = "buttonDeleno";
+            this.buttonDeleno.Size = new System.Drawing.Size(67, 61);
+            this.buttonDeleno.TabIndex = 13;
+            this.buttonDeleno.Text = "/";
+            this.buttonDeleno.UseVisualStyleBackColor = true;
+            // 
+            // buttonKrat
+            // 
+            this.buttonKrat.Location = new System.Drawing.Point(258, 133);
+            this.buttonKrat.Name = "buttonKrat";
+            this.buttonKrat.Size = new System.Drawing.Size(67, 61);
+            this.buttonKrat.TabIndex = 14;
+            this.buttonKrat.Text = "*";
+            this.buttonKrat.UseVisualStyleBackColor = true;
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(258, 200);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(67, 61);
+            this.buttonMinus.TabIndex = 15;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Location = new System.Drawing.Point(258, 267);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonPlus.Size = new System.Drawing.Size(67, 61);
+            this.buttonPlus.TabIndex = 16;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(22, 267);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(140, 61);
+            this.button0.TabIndex = 17;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // buttonVymazat
+            // 
+            this.buttonVymazat.Location = new System.Drawing.Point(168, 267);
+            this.buttonVymazat.Name = "buttonVymazat";
+            this.buttonVymazat.Size = new System.Drawing.Size(67, 61);
+            this.buttonVymazat.TabIndex = 18;
+            this.buttonVymazat.Text = "Vymazat";
+            this.buttonVymazat.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 340);
+            this.Controls.Add(this.buttonVymazat);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonKrat);
+            this.Controls.Add(this.buttonDeleno);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Kalkulačka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button buttonDeleno;
+        private System.Windows.Forms.Button buttonKrat;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonVymazat;
+    }
+}
+
